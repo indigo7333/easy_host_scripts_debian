4f78ae032c753e8929deda0e86aed51dba8ace6c
indigo7333 <indigo7333@gmail.com>
2015-04-18 22:21:24 +0300

    c

--- db_backup.sh
+++ db_backup.sh
@@ -1,8 +1,7 @@
-#usage $1=ftp_host $2=$users_backup $3=folder_name_ftp $4=ftp_pass $5=mysql_pass $6=hourly/daily/monthly
+#usage $1=ftp_host $2=folder_name_ftp $3=ftp_pass $4=mysql_pass $5=hourly/daily/monthly
 ftp_ip=$1
 folder_name_ftp=$3
 ftp_pass=$4
-users_backup=( $2 )
 MYSQL_PASS=$5
 FOLDER=$6
 #DBS=$(`mysql -u root -h localhost --password="$(MYSQL_PASS)" -Bse 'show databases'`)
@@ -17,15 +16,4 @@ do
  mysqldump -u root -h localhost --password="${mysql_pass}" $db | gzip -9 > /tmp/$FILE
  ftp-upload -h $ftp_ip --passive -u ftp_user --password $ftp_pass -d $host_name_ftp/$FOLDER/mysql/ /tmp/$FILE
  rm /tmp/$FILE
-done
-
-for user in "${users_backup[@]}"
-do
-	FILE=$user.tar.gz
-	echo "$(date)" > /home/$user/backup_date.txt
-	cd /home/$user
-	tar --exclude='.' -czf /tmp/$FILE *
-	ftp-upload -h $ftp_ip --passive -u ftp_user --password $ftp_pass -d $host_name_ftp/$FOLDER/files/$user /tmp/$FILE
-	rm /tmp/$FILE
-	rm /home/$user/backup_date.txt
 done
