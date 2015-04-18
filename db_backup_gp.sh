df5153c683dd913b7b5169e16b75ca9b4c03ed78
indigo7333 <indigo7333@gmail.com>
2015-04-18 22:48:08 +0300

    a

--- db_backup_gp.sh
+++ db_backup_gp.sh
@@ -1,9 +1,9 @@
 #usage $1=ftp_host $2=folder_name_ftp $3=ftp_pass $4=mysql_pass $5=hourly/daily/monthly  $6=pass
 ftp_ip=$1
-folder_name_ftp=$3
-ftp_pass=$4
-MYSQL_PASS=$5
-FOLDER=$6
+folder_name_ftp=$2
+ftp_pass=$3
+MYSQL_PASS=$4
+FOLDER=$5
 #DBS=$(`mysql -u root -h localhost --password="$(MYSQL_PASS)" -Bse 'show databases'`)
 DBS="$(mysql -u root -h localhost --password="$MYSQL_PASS" -Bse 'show databases')"
 for db in $DBS
