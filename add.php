48e8d58e207ac7cbed7c74c8258d176ca892cb8c
indigo7333 <indigo7333@gmail.com>
2015-08-24 08:45:36 +0300

    qweqwewqe
    qweqweqwe

--- add.php
+++ add.php
@@ -83,56 +83,35 @@ location ~* \.(jpg|jpeg|gif|png|ico|css|bmp|swf|js|txt|woff|woff2)$ {
 
 }
 ';
-if($ssl=="yes") {$config_nginx.="
-ssl_certificate /home/$user/ssl/$site_name/ssl.crt;
-ssl_certificate_key /home/$user/ssl/$site_name/ssl.key;
-ssl_stapling on;
-ssl_stapling_verify on;
-ssl_dhparam /home/$user/ssl/$site_name/dhparam.pem
-ssl_client_certificate /home/$user/ssl/$site_name/ssl.trusted;
-ssl_crl /home/$user/ssl/$site_name/ssl.trusted;
-ssl_trusted_certificate /home/$user/ssl/$site_name/ssl.trusted;
-ssl_prefer_server_ciphers on;
-resolver 8.8.8.8 8.8.4.4 valid=300s;
-ssl_session_tickets on;
-ssl_session_cache shared:SSL:50m;
-ssl_session_timeout 4h;
-
-";
-
-if($strong_ssl==2) { $config_nginx.="ssl_ciphers ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5;"; }
-else { $config_nginx.="ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';"; }
-
-
+if($ssl=="yes") 
+  {
+  $config_nginx.="
+  #ssl config
+  ssl_certificate /home/$user/ssl/$site_name/ssl.crt;
+  ssl_certificate_key /home/$user/ssl/$site_name/ssl.key;
+  ssl_stapling on;
+  ssl_stapling_verify on;
+  ssl_dhparam /home/$user/ssl/$site_name/dhparam.pem;
+  ssl_client_certificate /home/$user/ssl/$site_name/ssl.trusted;
+  ssl_crl /home/$user/ssl/$site_name/ssl.trusted;
+  ssl_trusted_certificate /home/$user/ssl/$site_name/ssl.trusted;
+  ssl_prefer_server_ciphers on;
+  resolver 8.8.8.8 8.8.4.4 valid=300s;
+  ssl_session_tickets on;
+  ssl_session_cache shared:SSL:50m;
+  ssl_session_timeout 4h;
+  ";
+  if($strong_ssl==2) { $config_nginx.="ssl_ciphers ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5;"; }
+  else { $config_nginx.="ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';"; }
+  $config_nginx.="  #ssl config";
+  }
 
 $config_nginx.='
 }
 ';
                       
 
-if($ssl=="yes")
- {
-  exec("mkdir /home/$user/ssl/");
-  exec("mkdir /home/$user/ssl/$site_name");
-  exec("chmod 777 -R /home/$user/ssl");
-  exec("chown www-data:$user -R /home/$user/ssl/");
-  echo "cat $cert $bundle > /home/$user/ssl/$site_name/ssl.crt";
-  exec("cat $cert $bundle > /home/$user/ssl/$site_name/ssl.crt");
-  exec("cat $bundle > /home/$user/ssl/$site_name/ssl.trusted");
-  exec("cp $key /home/$user/ssl/$site_name/ssl.key");
-  if(!file_exists("/home/$user/ssl/$site_name/dhparam.pem") )  { exec("openssl dhparam -out /home/$user/ssl/$site_name/dhparam.pem 2048"); }
- } 
-exec("mkdir /home/$user/www");
-exec("mkdir /home/$user/www/$site_name");
-exec("mkdir /home/$user/logs");
-exec("mkdir /home/$user/logs/apache");
-exec("mkdir /home/$user/logs/nginx");
-exec("mkdir /home/$user/mod-tmp");
-exec("chown www-data:$user /home/$user/mod-tmp");
-exec("chmod 777 -R /home/$user/mod-tmp");
-exec("chown -R www-data:$user /home/$user/logs"); 
-exec("chmod 777 -R /home/$user/logs");
-exec("chown $user:$user /home/$user/www");
+
 exec("chown $user:$user /home/$user/www/$site_name");
 
 
